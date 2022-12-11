4ccb3dd087d14412e343b021d5dc04a81cb63087
Kimal845 <tykvagdety13@gmail.com>
2022-12-11 17:04:21 +0300

    i`m stupid. i`m deleted settings file, but returned this file

--- sweet_donut/posts/models.py
+++ sweet_donut/posts/models.py
@@ -28,6 +28,7 @@ from django.conf import settings
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField(max_length=500, default=None, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
